30790c1f65c7f0c6bce4319d7197fb0702836e60
JOAO <joaoantoniocorreaf@gmail.com>
2023-05-22 23:38:09 -0300

    Inicialização do tabuleiro

--- jogo.c
+++ jogo.c
@@ -3,5 +3,14 @@
 
 int main()
 {
-    // É PRA FAZER O PROJETO!
+    char tabuleiro[3][3];
+    int linha, coluna, i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            tabuleiro[i][j] = ' ';
+        }
+    }
 }
